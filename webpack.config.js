b335f756da5e2069758c7f60b11452b9850cc337
Anthony Stanislaus <58376144+StanislausA@users.noreply.github.com>
2022-04-09 11:09:45 -0400

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    'index': path.resolve(__dirname, '/client/index.js')
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  mode: process.env.NODE_ENV,
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ]
+  },
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    historyApiFallback: true,
+    client: {
+      overlay: true
+    },
+    static: {
+      directory: path.resolve(__dirname, 'client'),
+      publicPath: '/'
+    },
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    compress: true,
+    proxy: {
+      context: ['/'],
+      target: 'http://localhost:3000' 
+    }
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, '/index.html')
+  })]
+};

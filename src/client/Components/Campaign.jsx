8795fffb6274e6cc17743753b96fad38d02bffef
Ethan Sclarsky <esclarsky@gmail.com>
2022-04-11 15:33:12 -0400

    working campaign

--- src/client/Components/Campaign.jsx
+++ src/client/Components/Campaign.jsx
@@ -4,6 +4,8 @@ const CampaignPage = props =>{
   // Deconstruct values off of props
   // const {name, company, posted_by, numComments, numVotes } = props.campaign;
 
+  const id=1
+  const fetchURL = '/campaigns/comments/'+ id;
 
   // Handles input boxes for storage of variable names
   const useInput = init => {
@@ -14,18 +16,42 @@ const CampaignPage = props =>{
     return [ value, onChange ];
   };
 
-
-
   // Stores anything typed into input boxes under proper variable names 
   const [ comment, commentOnChange ] = useInput('');
   
-  // 
-  const id=1
+  // Function for adding comment to database
+  const addComment = ()=>{
+    console.log('adding comment!')
+
+    if (comment === ''){console.log('no comment was input')}
+    if (comment !== ''){
+      const requestBody = {
+        comment
+      }
+      
+      fetch(fetchURL, {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'Application/JSON'
+        },
+        body: JSON.stringify(requestBody)
+      })  
+        .then(resp => resp.json())
+        .then(()=>{console.log(`success!`)})
+        .then(() => {
+          history.pushState({loggedIn: true}, "comment post success", './');
+          navigate("./", { state: { loginState: true, userId: 'tbd'}, replace: true });
+        })
+        .catch(err=>{console.log(err)})
+
+    }
+    else {return console.log('check errors above')}
+  }   
+
   
   // ON RENDER, will pull all comments from database for rendering
 
   const [data, setData] = useState(null);
-  const fetchURL = '/campaigns/comments/'+ id;
 
   useEffect(()=>{
     fetch(fetchURL, {
@@ -56,7 +82,7 @@ const CampaignPage = props =>{
             <label id="comment">Enter new comment</label>
             <input className='input' name="comment" placeholder="your comment here" onChange={commentOnChange}/>
           </div>
-        <button type="button" className="btnMain" >Add Comment</button>
+        <button type="button" className="btnMain" onClick={addComment}>Add Comment</button>
       </div>
     </main>
   );

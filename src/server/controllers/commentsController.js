8667499c24377287b6b42f5b768d5ae3d41156d5
Alvin Ma <alvin.ma95@gmail.com>
2022-04-11 18:24:35 -0400

    everything works?

--- src/server/controllers/commentsController.js
+++ src/server/controllers/commentsController.js
@@ -6,11 +6,7 @@ const comments = {};
 comments.getCommentsByCampaignId = async (req, res, next) => {
   const { id } = req.params;
   const text = `SELECT user_comments.*, users.username FROM user_comments INNER JOIN users ON user_comments.user_id = users.user_id WHERE campaign_id = ${id}`;
-  
-  const response = await db.query(text);
-  const rows = response.rows;
-
-  console.log(rows)
+  const {rows} = await db.query(text);
   res.locals.comments = rows;
   /** TODO: Add error handling */
   return next();
